3e1f6216a380ef47b4606d8522c4b7ad6c758944
PrinceRahul01 <119293777+PrinceRahul01@users.noreply.github.com>
2024-04-21 10:50:19 +0530

    first commit on 21 apr 2024 - learnt how to clean code and make a component

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,13 +1,11 @@
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Hello that's my first react app - Rahul Yadav
-        </h1>
-        I love my India
-      </header>
-    </div>
+    <>
+    <h1>
+        Hello that's my first react app - Rahul Yadav
+    </h1>
+      I love my India
+    </>
   );
 }
 export default App;
---
+++ app1/src/chai.jsx
@@ -0,0 +1,11 @@
+import App from './App'
+
+function Chai(){
+    return(
+        <>
+        <App/>
+        <h2>Chai bana kar maje lo </h2>
+        </>
+    )
+}
+export default Chai
